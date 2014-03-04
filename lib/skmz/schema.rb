921663e53412623b1ad926ff36bf7f3dc4e02aa3
Ryoji Yoshioka <yoshioka.ryoji@gmail.com>
2014-03-04 15:54:42 +0900

    Change syntax for class method

--- lib/skmz/schema.rb
+++ lib/skmz/schema.rb
@@ -3,23 +3,25 @@ module Skmz
     include ActiveModel::Model
     attr_accessor :tables
 
-    def self.load
-      schema = Skmz::Schema.new(tables: [])
-      table_names.each do |name|
-        schema.tables << Skmz::Table.new(name: name)
+    class << self
+      def load
+        schema = Skmz::Schema.new(tables: [])
+        table_names.each do |name|
+          schema.tables << Skmz::Table.new(name: name)
+        end
+        schema
       end
-      schema
+
+      private
+        def table_names
+          ActiveRecord::Base.connection.tables.delete_if do |table|
+            table == ActiveRecord::SchemaMigration.table_name
+          end
+        end
     end
 
     def as_json
       { tables: @tables.as_json }
     end
-
-    private
-      def self.table_names
-        ActiveRecord::Base.connection.tables.delete_if do |table|
-          table == ActiveRecord::SchemaMigration.table_name
-        end
-      end
   end
 end
